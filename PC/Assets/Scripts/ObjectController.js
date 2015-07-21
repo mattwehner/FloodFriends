7eeab1ed35015706eba131eb960a33aa2b06ac9c
Matt <mattwehnernz@gmail.com>
2015-07-21 13:41:53 +1200

    Initial PC Project Setup

---
+++ PC/Assets/Scripts/ObjectController.js
@@ -0,0 +1,26 @@
+﻿#pragma strict
+var movement = 0;
+var movementSpeed = 0.3;
+
+function Start () {
+
+};
+
+function Update () {
+    
+    if (Input.GetKeyDown(KeyCode.W)) {
+        movement = 1;
+    };
+    if (Input.GetKeyDown(KeyCode.S)) {
+        movement = 2;
+    };
+    if (movement == 0) {
+        transform.Translate(new Vector3(0,0,0));
+    };
+    if (movement == 1) {
+        transform.Translate(Vector3.forward * movementSpeed);
+    };
+    if (movement == 2) {
+        transform.Translate(Vector3.back * movementSpeed);
+    };
+};
