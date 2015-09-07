6d12cc11b987fd871398a06dbad0f46a035dc3d8
Matt Wehner <mattwehnernz@gmail.com>
2015-09-07 14:25:48 +1200

    Generating Level Exploration

---
+++ FloodBuds/Assets/Scripts/LevelList.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class LevelList
+    {
+
+        // Static singleton instance
+        private static LevelList _instance;
+
+        // Static singleton property
+        public static LevelList Instance
+        {
+            get { return _instance ?? (_instance = new LevelList()); }
+        }
+
+        public string[][] Levels = new string[][]
+        {
+            
+        };
+    }
+}
---
+++ FloodBuds/Assets/Scripts/MenuCommands.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class MenuCommands : MonoBehaviour {
+
+        public void GoToScene(int index)
+        {
+            Application.LoadLevel(index);
+        }
+
+        public void GoToScene(string levelName)
+        {
+            Application.LoadLevel(levelName);
+        }
+
+        public void Quit()
+        {
+            Application.Quit();
+        }
+
+        public void ResetLevel()
+        {
+            Application.LoadLevel(Application.loadedLevel);
+        }
+
+        public void NextLevel()
+        {
+            Application.LoadLevel(Application.loadedLevel + 1);
+        }
+
+        public void PreviousLevel()
+        {
+            Application.LoadLevel(Application.loadedLevel - 1);
+        }
+    }
+}
